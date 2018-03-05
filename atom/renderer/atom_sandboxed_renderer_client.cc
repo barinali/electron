72b790da89f01e9890b1005c84b851c98dccc3d2
Hari Juturu <jhreddy@microsoft.com>
2018-03-05 11:14:42 -0800

    Exposing process.env

--- atom/renderer/atom_sandboxed_renderer_client.cc
+++ atom/renderer/atom_sandboxed_renderer_client.cc
@@ -80,6 +80,177 @@ v8::Local<v8::Value> GetBinding(v8::Isolate* isolate, v8::Local<v8::String> key,
   return exports;
 }
 
+static void EnvGetter(Local<Name> property,
+                      const PropertyCallbackInfo<Value>& info) {
+  Isolate* isolate = info.GetIsolate();
+  if (property->IsSymbol()) {
+    return info.GetReturnValue().SetUndefined();
+  }
+#ifdef __POSIX__
+  node::Utf8Value key(isolate, property);
+  const char* val = getenv(*key);
+  if (val) {
+    return info.GetReturnValue().Set(String::NewFromUtf8(isolate, val));
+  }
+#else  // _WIN32
+  node::TwoByteValue key(isolate, property);
+  WCHAR buffer[32767];  // The maximum size allowed for environment variables.
+  SetLastError(ERROR_SUCCESS);
+  DWORD result = GetEnvironmentVariableW(reinterpret_cast<WCHAR*>(*key),
+                                         buffer,
+                                         arraysize(buffer));
+  // If result >= sizeof buffer the buffer was too small. That should never
+  // happen. If result == 0 and result != ERROR_SUCCESS the variable was not
+  // not found.
+  if ((result > 0 || GetLastError() == ERROR_SUCCESS) &&
+      result < arraysize(buffer)) {
+    const uint16_t* two_byte_buffer = reinterpret_cast<const uint16_t*>(buffer);
+    Local<String> rc = String::NewFromTwoByte(isolate, two_byte_buffer);
+    return info.GetReturnValue().Set(rc);
+  }
+#endif
+}
+
+
+static void EnvSetter(Local<Name> property,
+                      Local<Value> value,
+                      const PropertyCallbackInfo<Value>& info) {
+#ifdef __POSIX__
+  node::Utf8Value key(info.GetIsolate(), property);
+  node::Utf8Value val(info.GetIsolate(), value);
+  setenv(*key, *val, 1);
+#else  // _WIN32
+  node::TwoByteValue key(info.GetIsolate(), property);
+  node::TwoByteValue val(info.GetIsolate(), value);
+  WCHAR* key_ptr = reinterpret_cast<WCHAR*>(*key);
+  // Environment variables that start with '=' are read-only.
+  if (key_ptr[0] != L'=') {
+    SetEnvironmentVariableW(key_ptr, reinterpret_cast<WCHAR*>(*val));
+  }
+#endif
+  // Whether it worked or not, always return value.
+  info.GetReturnValue().Set(value);
+}
+
+
+static void EnvQuery(Local<Name> property,
+                     const PropertyCallbackInfo<Integer>& info) {
+  int32_t rc = -1;  // Not found unless proven otherwise.
+  if (property->IsString()) {
+#ifdef __POSIX__
+    node::Utf8Value key(info.GetIsolate(), property);
+    if (getenv(*key))
+      rc = 0;
+#else  // _WIN32
+    node::TwoByteValue key(info.GetIsolate(), property);
+    WCHAR* key_ptr = reinterpret_cast<WCHAR*>(*key);
+    SetLastError(ERROR_SUCCESS);
+    if (GetEnvironmentVariableW(key_ptr, nullptr, 0) > 0 ||
+        GetLastError() == ERROR_SUCCESS) {
+      rc = 0;
+      if (key_ptr[0] == L'=') {
+        // Environment variables that start with '=' are hidden and read-only.
+        rc = static_cast<int32_t>(v8::ReadOnly) |
+             static_cast<int32_t>(v8::DontDelete) |
+             static_cast<int32_t>(v8::DontEnum);
+      }
+    }
+#endif
+  }
+  if (rc != -1)
+    info.GetReturnValue().Set(rc);
+}
+
+
+static void EnvDeleter(Local<Name> property,
+                       const PropertyCallbackInfo<Boolean>& info) {
+  if (property->IsString()) {
+#ifdef __POSIX__
+    node::Utf8Value key(info.GetIsolate(), property);
+    unsetenv(*key);
+#else
+    node::TwoByteValue key(info.GetIsolate(), property);
+    WCHAR* key_ptr = reinterpret_cast<WCHAR*>(*key);
+    SetEnvironmentVariableW(key_ptr, nullptr);
+#endif
+  }
+
+  // process.env never has non-configurable properties, so always
+  // return true like the tc39 delete operator.
+  info.GetReturnValue().Set(true);
+}
+
+
+static void EnvEnumerator(const PropertyCallbackInfo<Array>& info) {
+  Environment* env = Environment::GetCurrent(info);
+  Isolate* isolate = env->isolate();
+  Local<Context> ctx = env->context();
+  Local<Function> fn = env->push_values_to_array_function();
+  Local<Value> argv[NODE_PUSH_VAL_TO_ARRAY_MAX];
+  size_t idx = 0;
+
+#ifdef __POSIX__
+  int size = 0;
+  while (environ[size])
+    size++;
+
+  Local<Array> envarr = Array::New(isolate);
+
+  for (int i = 0; i < size; ++i) {
+    const char* var = environ[i];
+    const char* s = strchr(var, '=');
+    const int length = s ? s - var : strlen(var);
+    argv[idx] = String::NewFromUtf8(isolate,
+                                    var,
+                                    String::kNormalString,
+                                    length);
+    if (++idx >= arraysize(argv)) {
+      fn->Call(ctx, envarr, idx, argv).ToLocalChecked();
+      idx = 0;
+    }
+  }
+  if (idx > 0) {
+    fn->Call(ctx, envarr, idx, argv).ToLocalChecked();
+  }
+#else  // _WIN32
+  WCHAR* environment = GetEnvironmentStringsW();
+  if (environment == nullptr)
+    return;  // This should not happen.
+  Local<Array> envarr = Array::New(isolate);
+  WCHAR* p = environment;
+  while (*p) {
+    WCHAR *s;
+    if (*p == L'=') {
+      // If the key starts with '=' it is a hidden environment variable.
+      p += wcslen(p) + 1;
+      continue;
+    } else {
+      s = wcschr(p, L'=');
+    }
+    if (!s) {
+      s = p + wcslen(p);
+    }
+    const uint16_t* two_byte_buffer = reinterpret_cast<const uint16_t*>(p);
+    const size_t two_byte_buffer_len = s - p;
+    argv[idx] = String::NewFromTwoByte(isolate,
+                                       two_byte_buffer,
+                                       String::kNormalString,
+                                       two_byte_buffer_len);
+    if (++idx >= arraysize(argv)) {
+      fn->Call(ctx, envarr, idx, argv).ToLocalChecked();
+      idx = 0;
+    }
+    p = s + wcslen(s) + 1;
+  }
+  if (idx > 0) {
+    fn->Call(ctx, envarr, idx, argv).ToLocalChecked();
+  }
+  FreeEnvironmentStringsW(environment);
+#endif
+
+  info.GetReturnValue().Set(envarr);
+}
+
 base::CommandLine::StringVector GetArgv() {
   return base::CommandLine::ForCurrentProcess()->argv();
 }
@@ -94,6 +265,20 @@ void InitializeBindings(v8::Local<v8::Object> binding,
   b.SetMethod("getArgv", GetArgv);
   b.SetMethod("getProcessMemoryInfo", &AtomBindings::GetProcessMemoryInfo);
   b.SetMethod("getSystemMemoryInfo", &AtomBindings::GetSystemMemoryInfo);
+   // create process.env
+  Local<ObjectTemplate> process_env_template =
+      ObjectTemplate::New(env->isolate());
+  process_env_template->SetHandler(NamedPropertyHandlerConfiguration(
+          EnvGetter,
+          EnvSetter,
+          EnvQuery,
+          EnvDeleter,
+          EnvEnumerator,
+          env->as_external()));
+
+  Local<Object> process_env =
+      process_env_template->NewInstance(env->context()).ToLocalChecked();
+  b.SetMethod("env", process_env);
 }
 
 class AtomSandboxedRenderViewObserver : public AtomRenderViewObserver {
